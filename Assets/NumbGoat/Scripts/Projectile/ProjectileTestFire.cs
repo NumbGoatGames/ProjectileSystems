19cef4e2a6676f685e2d5dcf9d567e8307e640c3
Timothy Gray <buzztimgray@gmail.com>
2017-09-08 00:31:24 +1200

    Working code, initial testing looks good.

--- Assets/NumbGoat/Scripts/Projectile/ProjectileTestFire.cs
+++ Assets/NumbGoat/Scripts/Projectile/ProjectileTestFire.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace NumbGoat.Projectile {
     /// <summary>
     ///     Controller class for testing projectile firing.
     /// </summary>
     public class ProjectileTestFire : MonoBehaviour {
+        public float Inaccuracy;
         public BaseProjectile Projectile;
         public bool Running = true;
-        public float ShootSpeed = 1f;
+        public float ShotSpeed = 1f;
+        private int targetCounter;
         public GameObject[] Targets;
-        public float Inaccuracy = 0f;
-        public bool UseNegAngle = false;
-        private int targetCounter = 0;
+        public bool UseNegAngle;
 
         public void Awake() {
             Physics.IgnoreLayerCollision(8, 8);
@@ -43,29 +42,60 @@ namespace NumbGoat.Projectile {
         }
 
         private void FireProjectile() {
+            #region ver1
+
+            //            GameObject targetGameObject = this.Targets[this.targetCounter];
+            //            //TODO Projectile pool(?)
+            //            Vector3 targetPosition = TrajectoryHelper.InterceptPosition(this.transform.position, Vector3.zero,
+            //                targetGameObject.transform.position, Vector3.zero, this.ShotSpeed);
+            //            targetPosition += this.GetRandomnessOfShot();
+            //            float? angle = TrajectoryHelper.CalculateProjectileAngle(this.gameObject.transform.position,
+            //                targetPosition, this.ShotSpeed, this.UseNegAngle);
+            //            if (angle == null) {
+            //                // if the angle couldn't be calculated, it probably means the target is too far away
+            //                // ie. it is impossible to hit the target with the current distance, projectile velocity and gravity
+            //                Debug.LogWarning(message: "Could not find angle to hit target.");
+            //                return;
+            //            }
+            //            float notNullAngle = angle.Value;
+            //            Debug.Log($"Got firing angle of {notNullAngle}");
+            //            BaseProjectile toFire = Instantiate(this.Projectile);
+            //            toFire.Target = targetGameObject; // Set the intended target of the projectile.
+            //            toFire.gameObject.SetActive(true); // Active the projectile (not needed if the prefab is already active).
+            //            toFire.transform.position = this.transform.position; // Set the position to the position of the shooter.
+            //            toFire.transform.LookAt(targetPosition); // Easiest way to get the projectile facing the target.
+            //            toFire.transform.rotation = Quaternion.Euler(notNullAngle, toFire.transform.eulerAngles.y,
+            //                toFire.transform.eulerAngles.z); // Look up at the correct angle.
+            //            toFire.Rigidbody.velocity = toFire.transform.forward * this.ShotSpeed; // Set the projectiles velocity.
+            //
+            //            if (++this.targetCounter >= this.Targets.Length) {
+            //                // Start from the beginning of the list again.
+            //                this.targetCounter = 0;
+            //            }
+
+            #endregion
+
             GameObject targetGameObject = this.Targets[this.targetCounter];
-            //TODO Projectile pool(?)
-            Vector3 targetPosition = TrajectoryHelper.InterceptPosition(this.transform.position, Vector3.zero,
-                targetGameObject.transform.position, Vector3.zero, this.ShootSpeed);
-            targetPosition += this.GetRandomnessOfShot();
-            float? angle = TrajectoryHelper.CalculateProjectileAngle(this.gameObject.transform.position,
-                targetPosition, this.ShootSpeed, this.UseNegAngle);
-            if (angle == null) {
-                // if the angle couldn't be calculated, it probably means the target is too far away
-                // ie. it is impossible to hit the target with the current distance, projectile velocity and gravity
-                Debug.LogWarning(message: "Could not find angle to hit target.");
-                return;
+            Transform targetTransform = targetGameObject.transform;
+
+            float distance = Vector3.Distance(this.transform.position, targetTransform.position);
+            float trajectoryAngle;
+
+            Vector3 targetCenter = TrajectoryHelper.FirstOrderIntercept(this.transform.position, Vector3.zero,
+                this.ShotSpeed, targetTransform.position, targetTransform.GetComponent<Rigidbody>().velocity);
+
+            if (TrajectoryHelper.CalculateTrajectory(distance, this.ShotSpeed, out trajectoryAngle)) {
+                float trajectoryHeight = Mathf.Tan(trajectoryAngle * Mathf.Deg2Rad) * distance;
+                targetCenter.y += trajectoryHeight;
             }
-            float notNullAngle = angle.Value;
-            Debug.Log($"Got firing angle of {notNullAngle}");
+
+            //fire at TargetCenter
             BaseProjectile toFire = Instantiate(this.Projectile);
             toFire.Target = targetGameObject; // Set the intended target of the projectile.
             toFire.gameObject.SetActive(true); // Active the projectile (not needed if the prefab is already active).
             toFire.transform.position = this.transform.position; // Set the position to the position of the shooter.
-            toFire.transform.LookAt(targetPosition); // Easiest way to get the projectile facing the target.
-            toFire.transform.rotation = Quaternion.Euler(notNullAngle, toFire.transform.eulerAngles.y,
-                toFire.transform.eulerAngles.z); // Look up at the correct angle.
-            toFire.Rigidbody.velocity = toFire.transform.forward * this.ShootSpeed; // Set the projectiles velocity.
+            toFire.transform.LookAt(targetCenter); // Easiest way to get the projectile facing the target.
+            toFire.Rigidbody.AddRelativeForce(0, 0, this.ShotSpeed, ForceMode.Impulse);
 
             if (++this.targetCounter >= this.Targets.Length) {
                 // Start from the beginning of the list again.
--- Assets/NumbGoat/Scripts/Projectile/TrajectoryHelper.cs
+++ Assets/NumbGoat/Scripts/Projectile/TrajectoryHelper.cs
@@ -5,6 +5,8 @@ namespace NumbGoat.Projectile {
     ///     Helper class for trajectory calculations.
     /// </summary>
     public static class TrajectoryHelper {
+        #region ver1
+
         /// <summary>
         ///     Calculates the angle needed to land a projectile on a given point.
         ///     Optionally needs gravity value (if different from global Physics gravity amount).
@@ -121,5 +123,76 @@ namespace NumbGoat.Projectile {
             // determinant == 0, one intercept path
             return Mathf.Max(-b / (2f * a), 0f);
         }
+
+        #endregion
+
+        #region ver2
+        // https://forum.unity3d.com/threads/projectile-trajectory-accounting-for-gravity-velocity-mass-distance.425560/#post-2750631
+
+        //first-order intercept using absolute target position
+        public static Vector3 FirstOrderIntercept(Vector3 shooterPosition, Vector3 shooterVelocity, float shotSpeed,
+            Vector3 targetPosition, Vector3 targetVelocity) {
+            Vector3 targetRelativePosition = targetPosition - shooterPosition;
+            Vector3 targetRelativeVelocity = targetVelocity - shooterVelocity;
+            float t = FirstOrderInterceptTime(
+                shotSpeed,
+                targetRelativePosition,
+                targetRelativeVelocity
+            );
+            return targetPosition + t * targetRelativeVelocity;
+        }
+
+        //first-order intercept using relative target position
+        public static float FirstOrderInterceptTime(float shotSpeed, Vector3 targetRelativePosition,
+            Vector3 targetRelativeVelocity) {
+            float velocitySquared = targetRelativeVelocity.sqrMagnitude;
+            if (velocitySquared < 0.001f) {
+                return 0f;
+            }
+
+            float a = velocitySquared - shotSpeed * shotSpeed;
+
+            //handle similar velocities
+            if (Mathf.Abs(a) < 0.001f) {
+                float t = -targetRelativePosition.sqrMagnitude /
+                          (2f * Vector3.Dot(targetRelativeVelocity, targetRelativePosition));
+                return Mathf.Max(t, 0f); //don't shoot back in time
+            }
+
+            float b = 2f * Vector3.Dot(targetRelativeVelocity, targetRelativePosition);
+            float c = targetRelativePosition.sqrMagnitude;
+            float determinant = b * b - 4f * a * c;
+
+            if (determinant > 0f) {
+                //determinant > 0; two intercept paths (most common)
+                float t1 = (-b + Mathf.Sqrt(determinant)) / (2f * a),
+                    t2 = (-b - Mathf.Sqrt(determinant)) / (2f * a);
+                if (t1 > 0f) {
+                    if (t2 > 0f) {
+                        return Mathf.Min(t1, t2); //both are positive
+                    }
+                    return t1; //only t1 is positive
+                }
+                return Mathf.Max(t2, 0f); //don't shoot back in time
+            }
+            if (determinant < 0f) {
+                //determinant < 0; no intercept path
+                return 0f;
+            }
+            return Mathf.Max(-b / (2f * a), 0f); //don't shoot back in time
+        }
+
+        public static bool CalculateTrajectory(float targetDistance, float projectileVelocity,
+            out float calculatedAngle) {
+            calculatedAngle = 0.5f * (Mathf.Asin(-Physics.gravity.y * targetDistance /
+                                                 (projectileVelocity * projectileVelocity)) * Mathf.Rad2Deg);
+            if (float.IsNaN(calculatedAngle)) {
+                calculatedAngle = 0;
+                return false;
+            }
+            return true;
+        }
+
+        #endregion
     }
 }
